b6a127294ffdbc9708a40d0a8c9648fb793bd2a8
Nazmul Himel <himel.cse96@gmail.com>
2024-07-07 02:23:19 +0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,17 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-      <HeroSection />
-      <Skill />
-    </div>
+      <div>
+          <HeroSection />
+          <Skill />
+          {/* https://github.com/Infvyr/nextjs-portfolio
+          https://github.com/Infvyr/Portfolio
+          https://github.com/iamshiv007/NextJS-Portfolio
+          https://github.com/iamhiman/personal-webpage-v2
+          https://github.com/said7388/developer-portfolio-nextjs
+          https://github.com/omunite215/Project_3DPortfolio
+          https://github.com/fazle-rabbi-dev/fazle-rabbi-dev.vercel.app
+          https://github.com/bbyc4kes/portfolio */}
+      </div>
   );
 }
