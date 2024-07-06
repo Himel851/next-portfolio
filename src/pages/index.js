7d9495ec7235a2f22d3b6de47c010d907007a94c
Nazmul Himel <himel.cse96@gmail.com>
2024-07-07 02:22:30 +0600

    skill

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import HeroSection from "@/components/HeroSection";
+import Skill from "@/components/Skill/Skill";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div>
       <HeroSection />
+      <Skill />
     </div>
   );
 }
