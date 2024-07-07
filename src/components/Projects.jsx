e7945f26da9ebdd402ba5b1bb113891d6fb7196a
Nazmul Himel <himel.cse96@gmail.com>
2024-07-08 01:06:04 +0600

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,39 +1,40 @@
+import Link from 'next/link';
 import React from 'react';
 
 
 const projectData = [
     {
-        image: '/path/to/hotel.jpg',
+        image: './images/hotel.jpg',
         title: 'Hotel Booking App',
         websiteLink: 'https://himelhotel851.netlify.app/',
         githubLink: 'https://github.com/Himel851/Hotel-Booking-App',
     },
     {
-        image: '/path/to/movie.jpg',
+        image: './images/movie.jpg',
         title: 'Movie App',
         websiteLink: 'https://movieapp851.netlify.app/',
         githubLink: 'https://github.com/Himel851/Movie-App',
     },
     {
-        image: '/path/to/ecom.jpg',
+        image: './images/ecom.png',
         title: 'E-Commerce Website',
         websiteLink: 'https://himel-shopping-cart851.netlify.app/',
         githubLink: 'https://github.com/Himel851/E-Commerece-Shopping-Cart',
     },
     {
-        image: '/path/to/youtube.jpg',
+        image: './images/youtube.PNG',
         title: 'YouTube Clone Application',
         websiteLink: 'https://himelyoutube851.netlify.app/',
         githubLink: 'https://github.com/Himel851/Youtube-Clone-Application',
     },
     {
-        image: '/path/to/diu.jpg',
+        image: './images/diu.jpeg',
         title: 'DIU',
         websiteLink: 'https://himel851.github.io/Daffodil-International-University/',
         githubLink: 'https://github.com/Himel851/Daffodil-International-University',
     },
     {
-        image: '/path/to/edu.jpg',
+        image: './images/edu.jpeg',
         title: 'Online Education',
         websiteLink: 'https://himel851.github.io/Online-Education/',
         githubLink: 'https://github.com/Himel851/Online-Education',
@@ -54,156 +55,31 @@ const Projects = () => {
 
                 {/* Container */}
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-
-                    {/* Grid Item */}
-                    <div
-                        style={{ backgroundImage: `url(${hotel})` }}
-                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-                    >
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                Hotel Booking App
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href='https://himelhotel851.netlify.app/' target="_blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Website
-                                    </button>
-                                </a>
-                                <a href='https://github.com/Himel851/Hotel-Booking-App' target="_blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Github
-                                    </button>
-                                </a>
+                    {projectData?.map((project, index) => (
+                        <div
+                        key={index}
+                            style={{ backgroundImage: `url(${project?.image})` }}
+                            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+                        >
+                            <div className='opacity-0 group-hover:opacity-100'>
+                                <span className='text-2xl font-bold text-white tracking-wider'>
+                                    {project?.title}
+                                </span>
+                                <div className='pt-8 text-center'>
+                                    <Link href={project?.websiteLink} target="_blank" rel="noopener noreferrer">
+                                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                            Website
+                                        </button>
+                                    </Link>
+                                    <Link href={project?.githubLink} target="_blank" rel="noopener noreferrer">
+                                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                            Github
+                                        </button>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
-
-                    <div
-                        style={{ backgroundImage: `url(${movie})` }}
-                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-                    >
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                Movie App
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href='https://movieapp851.netlify.app/' target="_blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Website
-                                    </button>
-                                </a>
-                                <a href='https://github.com/Himel851/Movie-App' target="_blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Github
-                                    </button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div
-                        style={{ backgroundImage: `url(${ecom})` }}
-                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-                    >
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                E-Commerce Website
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href='https://himel-shopping-cart851.netlify.app/' target="_blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Website
-                                    </button>
-                                </a>
-                                <a href='https://github.com/Himel851/E-Commerece-Shopping-Cart' target="_blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Github
-                                    </button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Grid Item */}
-                    <div
-                        style={{ backgroundImage: `url(${youtube})` }}
-                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-                    >
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                YouTube Clone Application
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href='https://himelyoutube851.netlify.app/' target="_blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Website
-                                    </button>
-                                </a>
-                                <a href='https://github.com/Himel851/Youtube-Clone-Application' target="_blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Github
-                                    </button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div
-                        style={{ backgroundImage: `url(${diu})` }}
-                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-                    >
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                DIU
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href='https://himel851.github.io/Daffodil-International-University/' target="_blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Website
-                                    </button>
-                                </a>
-                                <a href='https://github.com/Himel851/Daffodil-International-University' target="_blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Github
-                                    </button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Grid Item */}
-                    <div
-                        style={{ backgroundImage: `url(${edu})` }}
-                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-                    >
-                        {/* Hover Effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                Online Education
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href='https://himel851.github.io/Online-Education/' target="_blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Website
-                                    </button>
-                                </a>
-                                <a href='https://github.com/Himel851/Online-Education' target="_blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Github
-                                    </button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-
-
+                    ))}
                 </div>
             </div>
         </div>
