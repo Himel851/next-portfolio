72e1c43f8cde6e9b8cbdca3aec84c5561799ad6a
Nazmul Himel <himel.cse96@gmail.com>
2024-07-13 03:34:13 +0600

    skill

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,35 +1,54 @@
+import Image from 'next/image';
 import React from 'react';
+import Marquee from 'react-fast-marquee';
 
 const Skill = () => {
     const skills = {
         Frontend: [
             {
-                name: 'React',
-                src: '/images/react.png'
+                name: 'Next.js',
+                src: '/images/nextjs.png'
             },
             {
                 name: 'Redux',
                 src: '/images/redux.png'
             },
             {
-                name: 'Next.js',
-                src: '/images/next.png'
-            },
-            {
-                name: 'Storybook',
-                src: '/images/storybook.png'
+                name: 'React',
+                src: '/images/react.png'
             },
             {
                 name: 'React Router',
                 src: '/images/react-router.png'
+            },
+            
+            {
+                name: 'Tailwind CSS',
+                src: '/images/TAILWIND.png'
+            },
+            {
+                name: 'Node.js',
+                src: '/images/nodejs.png'
             },
             {
                 name: 'Sass (SCSS)',
                 src: '/images/sass.png'
             },
             {
-                name: 'jQuery',
-                src: '/images/jquery.png'
+                name: 'React Bootstrap',
+                src: '/images/react-bootstrap.jpg'
+            },
+            {
+                name: 'Bootstrap',
+                src: '/images/bootstrap.png'
+            },
+            {
+                name: 'CSS',
+                src: '/images/css.png'
+            },
+            {
+                name: 'Html',
+                src: '/images/html.png'
             }
         ],
         Languages: [
@@ -39,7 +58,7 @@ const Skill = () => {
             },
             {
                 name: 'C++',
-                src: '/images/cpp.png'
+                src: '/images/c++.png'
             },
             {
                 name: 'JavaScript',
@@ -47,11 +66,12 @@ const Skill = () => {
             },
             {
                 name: 'TypeScript',
-                src: '/images/typescript.png'
+                src: '/images/ts.png'
             }
         ],
        
     };
+
 
     return (
         <div name="skills" className="w-full h-fit bg-[#050816] text-gray-300 p-8">
@@ -63,9 +83,9 @@ const Skill = () => {
                 {Object.keys(skills).map((section) => (
                     <div key={section} className="mt-8">
                         <h2 className="text-2xl font-bold mb-4">{section}</h2>
-                        <div className="grid grid-cols-4z sm:grid-cols-4 gap-4">
+                        <div className="grid grid-cols-4z sm:grid-cols-8 gap-4">
                             {skills[section].map((skill, index) => (
-                                <div key={index} className="shadow-md shadow-[#040c16] hover:scale-105 duration-500 z-10 p-4 rounded-md bg-white">
+                                <div key={index} className="shadow-md shadow-[#040c16] hover:scale-105 duration-500 z-10 p-4 rounded-md bg-[#151030]">
                                     <img className="w-20 mx-auto" src={skill.src} alt={skill.name} />
                                     <p className="my-4 text-center">{skill.name}</p>
                                 </div>
